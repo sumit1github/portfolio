6fe2e07dcf62f543eebc61cb0d8185a41053dd03
sumit panda <sumit@sumits-MacBook-Air.local>
2025-09-15 16:38:40 +0530

    netlify

--- src/Portfolio/constant.js
+++ src/Portfolio/constant.js
@@ -120,7 +120,7 @@ export const projects = [
         is_details_present: true,
         overview: [
             {
-                image: ["src/assets/project_images/zope_to_django/img2.png"],
+                image: ["/assets/project_images/zope_to_django/img2.png"],
                 text: "Migration Dashboard",
                 keywords: "Zope, Django, Migration"
             }
@@ -136,7 +136,7 @@ export const projects = [
         is_details_present: true,
         overview: [
             {
-                image: ["/src/assets/project_images/ProductConfigurator/dashBoard.png",],
+                image: ["/assets/project_images/ProductConfigurator/dashBoard.png"],
                 text: `This project involved modernizing a legacy product configuration system originally built on .NET over a decade ago. We rebuilt the platform using Django and React to improve scalability, 
                 maintainability, and performance. It serves as a centralized product catalog for managing product data across multiple client applications. Key features include support for multi-country and 
                 multi-language configurations, dynamic form rendering via Product Templates and Product Updates, and advanced smart search functionality. The system enables full product lifecycle management and 
@@ -156,7 +156,7 @@ export const projects = [
                 ],
             },
             {
-                image: ["/src/assets/project_images/ProductConfigurator/ProductSearch.png"],
+                image: ["/assets/project_images/ProductConfigurator/ProductSearch.png"],
                 text: ` The platform offers a product search feature with a dynamic filter panel on the right-hand side. Users can select multiple categories and subcategories, and the results update automatically 
                 based on the selections. The system uses a dynamic category API that responds in real time, ensuring that all selected filters immediately reflect in the displayed product results.`,
                 keywords: [
@@ -169,7 +169,7 @@ export const projects = [
                 ,
             },
             {
-                image: ["/src/assets/project_images/ProductConfigurator/product-create.png",],
+                image: ["/assets/project_images/ProductConfigurator/product-create.png"],
                 text: `The "Create New Product" page offers a comprehensive set of features designed to streamline product configuration. Key functionalities include support for a Global Product Number, multiple alternative product images, 
                 and dynamic image loading based on the selected product template — which is displayed prominently at the top of the page. Additionally, the system supports media management through a dedicated media management interface, 
                 allowing you to upload PDF files and other assets. Products can be associated with specific categories, lifestyle groups, related products, and personalized recommendations — similar to the recommendation engine seen on 
@@ -184,7 +184,7 @@ export const projects = [
                 ,
             },
             {
-                image: ["/src/assets/project_images/ProductConfigurator/diagram.png"],
+                image: ["/assets/project_images/ProductConfigurator/diagram.png"],
                 text: `The system is API-based on the backend and supports integration with multiple cost platform applications. It also supports Single Sign-On (SSO), allowing the same authentication backend to be used seamlessly 
                 across product configuration, admin portals, and consumer-facing applications.`,
                 keywords: [
@@ -205,7 +205,7 @@ export const projects = [
         is_details_present: true,
         overview: [
             {
-                image: ["/src/assets/project_images/OneRequest/dashboard.png", "/src/assets/project_images/OneRequest/diaram.jpeg"],
+                image: ["/assets/project_images/OneRequest/dashboard.png", "/assets/project_images/OneRequest/diaram.jpeg"],
                 text: `This project involved building a centralized, API-driven administrative platform to manage and monitor multiple organizations from a unified interface. The application is designed with multi-tenant architecture, enabling granular control across organization-specific workflows, user hierarchies, and tax-related processes. 
                         Key capabilities include request lifecycle tracking, approval chains, spending analytics, and calendar integration to monitor compliance deadlines such as tax submissions or critical dates. It supports both custom and pre-built templates with flexible workflow configuration tailored to each organization’s structure and policies. 
                         The platform integrates with a product catalog where products can go through multiple approval stages before publishing. Features like Single Sign-On (SSO) ensure seamless authentication across modules including product configuration, admin panels, and consumer interfaces.
@@ -243,7 +243,7 @@ export const projects = [
         is_details_present: true,
         overview: [
             {
-                image: ["/src/assets/project_images/TopErp/DashBoard.png"],
+                image: ["/assets/project_images/TopErp/DashBoard.png"],
                 text: `This ERP system was developed for "Top Security Industries Private Limited," a pan-India security service provider specializing in manpower deployment across industrial facilities. The ERP serves as the core operational backbone, managing the entire lifecycle of guard assignment, attendance tracking, location monitoring, and duty scheduling across more than 400 factory sites nationwide.
 
                     Designed for scalability and high performance, the platform handles over 400+ requests per minute and integrates with multiple mobile applications available on the Play Store. These include GPS-based location tracking, real-time attendance capture, night shift monitoring, and field helper coordination tools.
@@ -266,7 +266,7 @@ export const projects = [
                 ]
             },
             {
-                image: ["/src/assets/project_images/TopErp/company_list.png"],
+                image: ["/assets/project_images/TopErp/company_list.png"],
                 text: `The Company Management module allows administrators to manage client organizations efficiently by adding, editing, and configuring client-specific data such as vacancies, salary by designation, and attendance QR codes. It also includes a robust document management system and the ability to assign organization-specific admin credentials, enabling clients to securely access transactions, communications, and operational details related to their account.`,
 
                 keywords: [
@@ -283,7 +283,7 @@ export const projects = [
                 ]
             },
             {
-                image: ["/src/assets/project_images/TopErp/Employeemanagement.png"],
+                image: ["/assets/project_images/TopErp/Employeemanagement.png"],
                 text: `The Employee Management module enables comprehensive control over employee data, including adding, updating, viewing, editing, deleting, and blocking employees. A key feature is employee movement tracking, allowing assignment of duties across multiple job sites within a month. It also includes customizable ID card generation with PVC print support, document management, and a Dress Management system for tracking uniforms and equipment issued over time. Additionally, administrators can generate official documents such as joining letters directly from the application.`,
 
                 keywords: [
@@ -301,7 +301,7 @@ export const projects = [
                 ]
             },
             {
-                image: ["/src/assets/project_images/TopErp/payroll.png"],
+                image: ["/assets/project_images/TopErp/payroll.png"],
                 text: `This is a dynamic and location-based Payroll Management System designed to handle complex salary structures for employees and security guards working across multiple job sites. Salaries are calculated on an hourly basis, varying per job site, and are derived from attendance data collected via a dedicated attendance application. The system factors in advance salary payments, uniform (dress) charges, mess deductions, and statutory deductions such as PF. It also allows salary disbursement from multiple employer bank accounts, with configurable account selection. Additionally, a 'Hold' feature enables salary to be calculated but withheld from final export, allowing employers to pause disbursement when required.`,
 
                 keywords: [
@@ -319,7 +319,7 @@ export const projects = [
                 ]
             },
             {
-                image: ["/src/assets/project_images/TopErp/Reports.png"],
+                image: ["/assets/project_images/TopErp/Reports.png"],
                 text: `The Report Management module enables the generation and export of multiple critical reports for payroll, audit, compliance, and inventory tracking. Key reports include the Bank Salary Report, which exports employee salary data in CSV or Excel format compatible with bank portals for bulk transfers. The Detailed Salary Report provides internal, audit-level visibility into all salary components for administrative use. Additional reports include the PF Report and ESIC Report, both aligned with Indian government regulations, for submission to the respective statutory portals.
 
                     The system also includes a Dress Report for monthly inventory tracking of uniforms and equipment issued to employees, helping monitor multiple set allocations. Lastly, the Joining Report provides insights into workforce onboarding trends, including joinings, dropouts, and candidate history over time.`,
@@ -340,7 +340,7 @@ export const projects = [
                 ]
             },
             {
-                image: ["/src/assets/project_images/TopErp/proposal.png"],
+                image: ["/assets/project_images/TopErp/proposal.png"],
                 text: `The Proposal Management module enables users to create and manage customized business proposals for potential clients. It offers pre-designed templates that can be reused, edited, or duplicated for faster proposal generation. Users can customize proposal content and layout, and send proposals directly via email from the platform using predefined email templates. This streamlines the client outreach process and ensures professional, consistent communication with prospects.`,
 
                 keywords: [
@@ -358,7 +358,7 @@ export const projects = [
 
             },
             {
-                image: ["/src/assets/project_images/TopErp/CMS.png"],
+                image: ["/assets/project_images/TopErp/CMS.png"],
                 text: `The Content Management System (CMS) allows administrators to dynamically manage website content across key sections such as the homepage, About Us, gallery, client logos, and blogs—optimized for SEO performance. It includes dedicated modules for job postings with location-based filtering and application tracking via the admin panel. 
 
                     Additional capabilities include managing service listings, privacy policy (linked to Google Play Store compliance for mobile apps), company vision, values, and internal blog articles that showcase organizational updates. All content is editable and designed to sync seamlessly with the frontend for real-time updates.`,
@@ -379,7 +379,7 @@ export const projects = [
                 ]
             },
             {
-                image: ["/src/assets/project_images/TopErp/sysdiagram.png"],
+                image: ["/assets/project_images/TopErp/sysdiagram.png"],
                 text: `The platform is powered by a robust backend architecture built primarily using Django and FastAPI. The core application is hosted on AWS EC2, with seamless integrations to various AWS services to ensure scalability and performance. AWS SQS is used for event-based job queuing, S3 handles file storage and media uploads, and SES is utilized for email communications.
 
                         -intensive operations like attendance processing are offloaded to AWS Lambda for asynchronous execution. Attendance and workflow applications push events to SQS, which are then processed by Lambda functions and persisted into Amazon RDS. This distributed architecture ensures optimal load balancing and high availability across the system.`,
